dc366600958559a31cc093197b19327f21af8c70
RekGRpth <rekgrpth@gmail.com>
2019-03-27 16:19:10 +0500

    up

--- pg_curl.c
+++ pg_curl.c
@@ -131,10 +131,11 @@ Datum pg_curl_easy_getinfo_str(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(pg_curl_ea
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("argument info must not null!")));
     if (!curl) ereport(ERROR, (errmsg("call pg_curl_easy_init before!")));
     info_str = text_to_cstring(PG_GETARG_TEXT_P(0));
+    if (!pg_strncasecmp(info_str, "CURLINFO_RESPONSE", sizeof("CURLINFO_RESPONSE") - 1)) { str = data.data; goto ret; }
     if (!pg_strncasecmp(info_str, "CURLINFO_CONTENT_TYPE", sizeof("CURLINFO_CONTENT_TYPE") - 1)) info = CURLINFO_CONTENT_TYPE;
     if (info == CURLINFO_NONE) ereport(ERROR, (errmsg("unsupported option %s", info_str)));
     if ((res = curl_easy_getinfo(curl, info, &str)) != CURLE_OK) ereport(ERROR, (errmsg("curl_easy_getinfo(%s): %s", info_str, curl_easy_strerror(res))));
-    if (!str) PG_RETURN_NULL();
+    ret: if (!str) PG_RETURN_NULL();
     PG_RETURN_TEXT_P(cstring_to_text(str));
 }
 
