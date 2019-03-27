5f0c0aa7f4f074aed0016fc6716e11349f8a2052
RekGRpth <rekgrpth@gmail.com>
2019-03-27 14:11:29 +0500

    up

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-MODULES = pg_curl
 EXTENSION = pg_curl
+MODULE_big = $(EXTENSION)
+
 CURL_CONFIG = curl-config
 PG_CONFIG = pg_config
+OBJS = $(EXTENSION).o
 DATA = pg_curl--1.0.sql
 
 CFLAGS += $(shell $(CURL_CONFIG) --cflags)
--- pg_curl--1.0.sql
+++ pg_curl--1.0.sql
@@ -1,5 +1,5 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION http" to load this file. \quit
+\echo Use "CREATE EXTENSION pg_curl" to load this file. \quit
 
 CREATE OR REPLACE FUNCTION pg_curl_easy_init() RETURNS boolean AS 'MODULE_PATHNAME', 'pg_curl_easy_init' LANGUAGE 'c';
 
