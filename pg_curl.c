f8ad6f541433f690630edb5ece607539e381f8ab
RekGRpth <rekgrpth@gmail.com>
2019-03-27 14:50:30 +0500

    up

--- pg_curl.c
+++ pg_curl.c
@@ -62,10 +62,35 @@ Datum pg_curl_easy_reset(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(pg_curl_easy_res
 
 Datum pg_curl_easy_setopt(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(pg_curl_easy_setopt); Datum pg_curl_easy_setopt(PG_FUNCTION_ARGS) {
     CURLcode res;
+    CURLoption option = CURLOPT_LASTENTRY;
+    char *option_str;
+    char *parameter_str = NULL;
+    int len = sizeof("CURLOPT_") - 1;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("pg_curl_easy_setopt: PG_ARGISNULL(0)"), errhint("arg option must not null!")));
     if (PG_ARGISNULL(1)) ereport(ERROR, (errmsg("pg_curl_easy_setopt: PG_ARGISNULL(1)"), errhint("arg parameter must not null!")));
     if (!curl) ereport(ERROR, (errmsg("pg_curl_easy_setopt: !curl"), errhint("call pg_curl_easy_init before!")));
-    if ((res = curl_easy_setopt(curl, CURLOPT_URL, "http://example.com")) != CURLE_OK) ereport(ERROR, (errmsg("curl_easy_setopt: %s", curl_easy_strerror(res))));
+    option_str = text_to_cstring(PG_GETARG_TEXT_P(0));
+    if (strlen(option_str) <= len) ereport(ERROR, (errmsg("pg_curl_easy_setopt: strlen"), errhint("arg option length must greater!")));
+    if (pg_strncasecmp(option_str, "CURLOPT_", len)) ereport(ERROR, (errmsg("pg_curl_easy_setopt: pg_strncasecmp"), errhint("arg option must starts with CURLOPT_")));
+    switch (option_str[len++]) {
+        case 'u':
+        case 'U': {
+            switch (option_str[len++]) {
+                case 'r':
+                case 'R': {
+                    switch (option_str[len++]) {
+                        case 'l':
+                        case 'L': {
+                            option = CURLOPT_URL;
+                            parameter_str = text_to_cstring(PG_GETARG_TEXT_P(1));
+                        } break;
+                    }
+                } break;
+            }
+        } break;
+    }
+    if (option == CURLOPT_LASTENTRY) ereport(ERROR, (errmsg("pg_curl_easy_setopt: option == CURLOPT_LASTENTRY"), errhint("unsupported option %s", option_str)));
+    if ((res = curl_easy_setopt(curl, option, parameter_str)) != CURLE_OK) ereport(ERROR, (errmsg("curl_easy_setopt: %s", curl_easy_strerror(res))));
     PG_RETURN_BOOL(res == CURLE_OK);
 }
 
