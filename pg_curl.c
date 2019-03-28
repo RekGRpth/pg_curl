2d0e636fcb6529da32a343bc5c30379dce343fbe
RekGRpth <rekgrpth@gmail.com>
2019-03-28 15:33:18 +0500

    up

--- pg_curl.c
+++ pg_curl.c
@@ -231,8 +231,9 @@ Datum pg_curl_easy_getinfo_char(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(pg_curl_e
     if (!curl) ereport(ERROR, (errmsg("call pg_curl_easy_init before!")));
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("argument info must not null!")));
     info_char = TextDatumGetCString(PG_GETARG_DATUM(0));
-    if (!pg_strncasecmp(info_char, "CURLINFO_RESPONSE", sizeof("CURLINFO_RESPONSE") - 1)) { str = write_buf.data; goto ret; }
+    if (false);
     else if (!pg_strncasecmp(info_char, "CURLINFO_CONTENT_TYPE", sizeof("CURLINFO_CONTENT_TYPE") - 1)) info = CURLINFO_CONTENT_TYPE;
+    else if (!pg_strncasecmp(info_char, "CURLINFO_RESPONSE", sizeof("CURLINFO_RESPONSE") - 1)) { str = write_buf.data; goto ret; }
     else ereport(ERROR, (errmsg("unsupported option %s", info_char)));
     if ((res = curl_easy_getinfo(curl, info, &str)) != CURLE_OK) ereport(ERROR, (errmsg("curl_easy_getinfo(%s): %s", info_char, curl_easy_strerror(res))));
 ret:
@@ -249,7 +250,8 @@ Datum pg_curl_easy_getinfo_long(PG_FUNCTION_ARGS); PG_FUNCTION_INFO_V1(pg_curl_e
     if (!curl) ereport(ERROR, (errmsg("call pg_curl_easy_init before!")));
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("argument info must not null!")));
     info_char = TextDatumGetCString(PG_GETARG_DATUM(0));
-    if (!pg_strncasecmp(info_char, "CURLINFO_RESPONSE_CODE", sizeof("CURLINFO_RESPONSE_CODE") - 1)) info = CURLINFO_RESPONSE_CODE;
+    if (false);
+    else if (!pg_strncasecmp(info_char, "CURLINFO_RESPONSE_CODE", sizeof("CURLINFO_RESPONSE_CODE") - 1)) info = CURLINFO_RESPONSE_CODE;
     else ereport(ERROR, (errmsg("unsupported option %s", info_char)));
     if ((res = curl_easy_getinfo(curl, info, &lon)) != CURLE_OK) ereport(ERROR, (errmsg("curl_easy_getinfo(%s): %s", info_char, curl_easy_strerror(res))));
     (void)pfree(info_char);
