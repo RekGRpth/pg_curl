e90d854a13bf1585268c1bc05e332677652c2166
RekGRpth <rekgrpth@gmail.com>
2019-07-07 14:44:23 +0500

    up

--- pg_curl.c
+++ pg_curl.c
@@ -206,20 +206,21 @@ EXTENSION(pg_curl_recipient_append_array) {
 
 EXTENSION(pg_curl_mime_data) {
     CURLcode res = CURL_LAST;
-    text *data, *name = NULL, *file = NULL, *type = NULL, *code = NULL;
+    text *data;
+    char *name = NULL, *file = NULL, *type = NULL, *code = NULL;
     curl_mimepart *part;
     if (PG_ARGISNULL(0)) ereport(ERROR, (errmsg("data is null!")));
     data = PG_GETARG_TEXT_P(0);
-    if (!PG_ARGISNULL(1)) name = PG_GETARG_TEXT_P(1);
-    if (!PG_ARGISNULL(2)) file = PG_GETARG_TEXT_P(2);
-    if (!PG_ARGISNULL(3)) type = PG_GETARG_TEXT_P(3);
-    if (!PG_ARGISNULL(4)) code = PG_GETARG_TEXT_P(4);
+    if (!PG_ARGISNULL(1)) name = TextDatumGetCString(PG_GETARG_DATUM(1));
+    if (!PG_ARGISNULL(2)) file = TextDatumGetCString(PG_GETARG_DATUM(2));
+    if (!PG_ARGISNULL(3)) type = TextDatumGetCString(PG_GETARG_DATUM(3));
+    if (!PG_ARGISNULL(4)) code = TextDatumGetCString(PG_GETARG_DATUM(4));
     part = curl_mime_addpart(mime);
     if ((res = curl_mime_data(part, VARDATA_ANY(data), VARSIZE_ANY_EXHDR(data))) != CURLE_OK) ereport(ERROR, (errmsg("curl_mime_data(%s): %s", VARDATA_ANY(data), curl_easy_strerror(res))));
-    if (name && ((res = curl_mime_name(part, VARDATA_ANY(name))) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_name(%s): %s", VARDATA_ANY(name), curl_easy_strerror(res))));
-    if (file && ((res = curl_mime_filename(part, VARDATA_ANY(file))) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_filename(%s): %s", VARDATA_ANY(file), curl_easy_strerror(res))));
-    if (type && ((res = curl_mime_type(part, VARDATA_ANY(type))) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_type(%s): %s", VARDATA_ANY(type), curl_easy_strerror(res))));
-    if (code && ((res = curl_mime_encoder(part, VARDATA_ANY(code))) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_encoder(%s): %s", VARDATA_ANY(code), curl_easy_strerror(res))));
+    if (name && ((res = curl_mime_name(part, name)) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_name(%s): %s", name, curl_easy_strerror(res))));
+    if (file && ((res = curl_mime_filename(part, file)) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_filename(%s): %s", file, curl_easy_strerror(res))));
+    if (type && ((res = curl_mime_type(part, type)) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_type(%s): %s", type, curl_easy_strerror(res))));
+    if (code && ((res = curl_mime_encoder(part, code)) != CURLE_OK)) ereport(ERROR, (errmsg("curl_mime_encoder(%s): %s", code, curl_easy_strerror(res))));
     (void)pfree(data);
     if (name) (void)pfree(name);
     if (file) (void)pfree(file);
