5b81b7e0b55bf3fa35a2879ec06dc3eda43bc4ad
RekGRpth <rekgrpth@gmail.com>
2021-12-22 14:53:52 +0500

    test

--- sql/http_methods.sql
+++ sql/http_methods.sql
@@ -24,4 +24,13 @@ with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server');
 select  jsonb_pretty(jsonb_set_lax(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'), '{origin}', null, true, 'delete_key'));
+
+select curl_easy_reset();
+select curl_easy_setopt_postfields(convert_to('e=f&g=&h', 'utf-8'));
+select curl_easy_setopt_url('https://httpbin.org/post?a=b&c=&d');
+select curl_easy_perform();
+with s as (
+    select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
+) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server');
+select  jsonb_pretty(jsonb_set_lax(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'), '{origin}', null, true, 'delete_key'));
 ROLLBACK;
