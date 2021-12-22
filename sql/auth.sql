34de4054bf65508fbd6cb0a9914a43d589d26976
RekGRpth <rekgrpth@gmail.com>
2021-12-22 15:30:12 +0500

    test

--- sql/auth.sql
+++ sql/auth.sql
@@ -26,4 +26,13 @@ with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server');
 select convert_from(curl_easy_getinfo_data_in(), 'utf-8');
+
+select curl_easy_reset();
+select curl_easy_setopt_url('https://httpbin.org/bearer');
+select curl_header_append('Authorization', 'Bearer token');
+select curl_easy_perform();
+with s as (
+    select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
+) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server');
+select convert_from(curl_easy_getinfo_data_in(), 'utf-8');
 ROLLBACK;
