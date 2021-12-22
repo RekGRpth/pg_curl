bccd4b434660be4224f8eceac951a0a05322dec9
RekGRpth <rekgrpth@gmail.com>
2021-12-22 14:51:01 +0500

    test

--- sql/http_methods.sql
+++ sql/http_methods.sql
@@ -13,7 +13,7 @@ select curl_easy_perform();
 with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date');
-select  jsonb_pretty(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'));
+select  jsonb_pretty(jsonb_set_lax(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'), '{origin}', null, true, 'delete_key'));
 
 select curl_easy_reset();
 select curl_easy_setopt_postfields(convert_to('{"e":"f","g":"","h":null}', 'utf-8'));
@@ -23,5 +23,5 @@ select curl_easy_perform();
 with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date');
-select  jsonb_pretty(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'));
+select  jsonb_pretty(jsonb_set_lax(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'), '{origin}', null, true, 'delete_key'));
 ROLLBACK;
