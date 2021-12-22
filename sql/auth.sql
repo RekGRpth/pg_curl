8809d2ebdfa85e4f8002d3ac20f8ad03fe29ca95
RekGRpth <rekgrpth@gmail.com>
2021-12-22 15:22:53 +0500

    test

---
+++ sql/auth.sql
@@ -0,0 +1,19 @@
+\unset ECHO
+\set QUIET 1
+\pset format unaligned
+\pset tuples_only true
+\pset pager off
+\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_STOP true
+BEGIN;
+CREATE EXTENSION pg_curl;
+select curl_easy_reset();
+select curl_easy_setopt_password('password');
+select curl_easy_setopt_url('https://httpbin.org/basic-auth/username/password');
+select curl_easy_setopt_username('username');
+select curl_easy_perform();
+with s as (
+    select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
+) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server');
+select  jsonb_pretty(jsonb_set_lax(jsonb_set_lax(convert_from(curl_easy_getinfo_data_in(), 'utf-8')::jsonb, '{headers,X-Amzn-Trace-Id}', null, true, 'delete_key'), '{origin}', null, true, 'delete_key'));
+ROLLBACK;
