531c262d2f140fcec44b1b70a73410c80cf19528
RekGRpth <rekgrpth@gmail.com>
2023-05-31 19:54:21 +0500

    test

--- sql/auth.sql
+++ sql/auth.sql
@@ -16,6 +16,7 @@ with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server', 'content-length');
 select convert_from(curl_easy_getinfo_data_in(), 'utf-8');
+select curl_easy_getinfo_errcode(), curl_easy_getinfo_errbuff();
 select curl_easy_reset();
 select curl_easy_setopt_password('password');
 select curl_easy_setopt_url('https://httpbin.org/basic-auth/username/password');
@@ -25,6 +26,7 @@ with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server', 'content-length');
 select convert_from(curl_easy_getinfo_data_in(), 'utf-8');
+select curl_easy_getinfo_errcode(), curl_easy_getinfo_errbuff();
 select curl_easy_reset();
 select curl_easy_setopt_url('https://httpbin.org/bearer');
 select curl_header_append('Authorization', 'Bearer token');
@@ -33,4 +35,5 @@ with s as (
     select regexp_matches(curl_easy_getinfo_header_in(), E'([^ \t\r\n\f]+): ?([^\t\r\n\f]+)', 'g') as s
 ) select s[1] as key, s[2] as value from s where s[1] not in ('date', 'server', 'content-length');
 select convert_from(curl_easy_getinfo_data_in(), 'utf-8');
+select curl_easy_getinfo_errcode(), curl_easy_getinfo_errbuff();
 ROLLBACK;
