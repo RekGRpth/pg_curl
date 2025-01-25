8c431ff80e8fb7cf1d1cb07096f2c35e54ad62c4
RekGRpth <rekgrpth@gmail.com>
2025-01-25 19:46:32 +0500

    speedup

--- sql/interrupt.sql
+++ sql/interrupt.sql
@@ -7,7 +7,7 @@
 BEGIN;
 CREATE EXTENSION pg_curl;
 select curl_easy_reset();
-select curl_easy_setopt_url('https://httpbin.org/delay/10');
-set statement_timeout = '5s';
+select curl_easy_setopt_url('https://httpbin.org/delay/2');
+set statement_timeout = '1s';
 select curl_easy_perform();
 ROLLBACK;
--- sql/interrupt_.sql
+++ sql/interrupt_.sql
@@ -6,8 +6,8 @@
 \set ON_ERROR_ROLLBACK 1
 BEGIN;
 CREATE EXTENSION pg_curl;
-select curl_easy_setopt_url('https://httpbin.org/delay/10', conname:='1');
-select curl_easy_setopt_url('https://httpbin.org/delay/7', conname:='2');
-set statement_timeout = '5s';
+select curl_easy_setopt_url('https://httpbin.org/delay/2', conname:='1');
+select curl_easy_setopt_url('https://httpbin.org/delay/3', conname:='2');
+set statement_timeout = '1s';
 select curl_multi_perform();
 ROLLBACK;
