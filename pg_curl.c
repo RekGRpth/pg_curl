b5daf0486e909215cff149ed4a26a2f57ba7a705
RekGRpth <rekgrpth@gmail.com>
2025-02-07 10:28:42 +0500

    rename

--- pg_curl.c
+++ pg_curl.c
@@ -43,7 +43,7 @@ typedef struct {
 typedef struct {
     char conname[NAMEDATALEN]; // always first, because it is key for hashmap
     pg_curl_t *curl;
-} pg_easy_t;
+} pg_curl_hash_t;
 
 static struct {
     bool transaction;
@@ -226,11 +226,11 @@ static pg_curl_t *pg_curl_easy_init(const char *conname) {
 #endif
     MemoryContext oldMemoryContext;
     pg_curl_t *curl;
-    pg_easy_t *easy;
+    pg_curl_hash_t *hash;
     pg_curl_multi_init();
-    easy = hash_search(pg_curl.hash, conname, HASH_ENTER, &found);
-    if (!found) easy->curl = MemoryContextAllocZero(pg_curl.context, sizeof(*easy->curl));
-    curl = easy->curl;
+    hash = hash_search(pg_curl.hash, conname, HASH_ENTER, &found);
+    if (!found) hash->curl = MemoryContextAllocZero(pg_curl.context, sizeof(*hash->curl));
+    curl = hash->curl;
     if (curl->easy) return curl;
     oldMemoryContext = MemoryContextSwitchTo(pg_curl.context);
     initStringInfo(&curl->data_in);
@@ -1817,14 +1817,14 @@ EXTENSION(pg_curl_multi_perform) {
     int timeout_ms;
     int try;
     long sleep;
-    pg_easy_t *easy;
+    pg_curl_hash_t *hash;
     if (!pg_curl.hash) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("curl_multi_perform/curl_easy_perform no connections"), errhint("curl_multi_perform/curl_easy_perform requires at least one connection!")));
     if ((try = PG_ARGISNULL(0) ? 1 : PG_GETARG_INT32(0)) <= 0) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("curl_multi_perform invalid argument try %i", try), errhint("Argument try must be positive!")));
     if ((sleep = PG_ARGISNULL(1) ? 1000000 : PG_GETARG_INT64(1)) < 0) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("curl_multi_perform invalid argument sleep %li", sleep), errhint("Argument sleep must be non-negative!")));
     if ((timeout_ms = PG_ARGISNULL(2) ? 1000 : PG_GETARG_INT32(0)) <= 0) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("curl_multi_perform invalid argument timeout_ms %i", timeout_ms), errhint("Argument timeout_ms must be positive!")));
     hash_seq_init(&status, pg_curl.hash);
-    while ((easy = hash_seq_search(&status))) {
-        pg_curl_t *curl = easy->curl;
+    while ((hash = hash_seq_search(&status))) {
+        pg_curl_t *curl = hash->curl;
         if ((ec = pg_curl_easy_prepare(curl)) != CURLE_OK) ereport(ERROR, (pg_curl_ec(ec), errmsg("%s", curl_easy_strerror(ec))));
         if ((mc = curl_multi_add_handle(curl->multi = pg_curl.multi, curl->easy)) != CURLM_OK) ereport(ERROR, (pg_curl_mc(mc), errmsg("%s", curl_multi_strerror(mc))));
     }
